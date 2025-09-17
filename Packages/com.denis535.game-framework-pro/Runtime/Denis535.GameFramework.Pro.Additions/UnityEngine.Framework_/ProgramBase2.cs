330006b0e5d760e041642aefb8e7b47710c6436e
Denis535 <dddenisss35@gmail.com>
2025-09-17 08:41:12 +0300

    GameFramework.Pro

--- Packages/com.denis535.game-framework-pro/Runtime/Denis535.GameFramework.Pro.Additions/UnityEngine.Framework_/ProgramBase2.cs
+++ Packages/com.denis535.game-framework-pro/Runtime/Denis535.GameFramework.Pro.Additions/UnityEngine.Framework_/ProgramBase2.cs
@@ -38,9 +38,7 @@ namespace UnityEngine.Framework {
         Option<object?> IDependencyContainer.GetValue(Type type, object? argument) {
             return this.GetValue( type, argument );
         }
-        protected virtual Option<object?> GetValue(Type type, object? argument) {
-            return default;
-        }
+        protected abstract Option<object?> GetValue(Type type, object? argument);
 
     }
     public abstract partial class ProgramBase2<TTheme, TScreen, TRouter, TApplication, TGame> : ProgramBase2
@@ -51,10 +49,10 @@ namespace UnityEngine.Framework {
         where TGame : notnull, GameBase {
 
         // Framework
-        protected abstract TTheme Theme { get; set; }
-        protected abstract TScreen Screen { get; set; }
-        protected abstract TRouter Router { get; set; }
-        protected abstract TApplication Application { get; set; }
+        protected abstract TTheme Theme { get; }
+        protected abstract TScreen Screen { get; }
+        protected abstract TRouter Router { get; }
+        protected abstract TApplication Application { get; }
         protected abstract TGame? Game { get; }
 
         // Awake
@@ -82,28 +80,72 @@ namespace UnityEngine.Framework {
 
         // GetValue
         protected override Option<object?> GetValue(Type type, object? argument) {
-            return base.GetValue( type, argument );
+            this.ThrowIfInvalid();
+            // UI
+            if (type == typeof( TTheme )) {
+                if (this.Theme != null) return Option.Create( (object?) this.Theme );
+                return default;
+            }
+            if (type == typeof( TScreen )) {
+                if (this.Screen != null) return Option.Create( (object?) this.Screen );
+                return default;
+            }
+            if (type == typeof( TRouter )) {
+                if (this.Router != null) return Option.Create( (object?) this.Router );
+                return default;
+            }
+            // App
+            if (type == typeof( TApplication )) {
+                if (this.Application != null) return Option.Create( (object?) this.Application );
+                return default;
+            }
+            // Game
+            if (type == typeof( TGame )) {
+                if (this.Game != null) return Option.Create( (object?) this.Game );
+                return default;
+            }
+            return default;
         }
 
 #if UNITY_EDITOR
         // OnInspectorGUI
         protected internal override void OnInspectorGUI() {
             if (this.didAwake && this) {
-                this.OnInspectorGUI( this.Theme, this.Screen, this.Router, this.Application, this.Game );
+                this.OnInspectorGUI( this.Theme );
+                this.OnInspectorGUI( this.Screen );
+                this.OnInspectorGUI( this.Router );
+                this.OnInspectorGUI( this.Application );
+                this.OnInspectorGUI( this.Game );
             } else {
                 HelpBox.Draw();
             }
         }
-        protected virtual void OnInspectorGUI(ThemeBase theme, ScreenBase screen, RouterBase router, ApplicationBase application, GameBase? game) {
-            LabelField( "Theme", theme.ToString() );
-            LabelField( "PlayList", theme.Machine.Root?.PlayList().Pipe( GetDisplayString ) ?? "Null" );
-            GUILayout.Space( 2 );
-            LabelField( "Screen", screen.ToString() );
-            LabelField( "Widget", screen.Machine.Root?.Widget().Pipe( GetDisplayString ) ?? "Null" );
-            GUILayout.Space( 2 );
-            LabelField( "Router", router.ToString() );
-            LabelField( "Application", application.ToString() );
-            LabelField( "Game", game?.ToString() ?? "Null" );
+        protected virtual void OnInspectorGUI(ThemeBase theme) {
+            using (new GUILayout.VerticalScope( EditorStyles.helpBox )) {
+                LabelField( "Theme", theme.ToString() );
+                LabelField( "PlayList", theme.Machine.Root?.PlayList().Pipe( GetDisplayString ) ?? "Null" );
+            }
+        }
+        protected virtual void OnInspectorGUI(ScreenBase screen) {
+            using (new GUILayout.VerticalScope( EditorStyles.helpBox )) {
+                LabelField( "Screen", screen.ToString() );
+                LabelField( "Widget", screen.Machine.Root?.Widget().Pipe( GetDisplayString ) ?? "Null" );
+            }
+        }
+        protected virtual void OnInspectorGUI(RouterBase router) {
+            using (new GUILayout.VerticalScope( EditorStyles.helpBox )) {
+                LabelField( "Router", router.ToString() );
+            }
+        }
+        protected virtual void OnInspectorGUI(ApplicationBase application) {
+            using (new GUILayout.VerticalScope( EditorStyles.helpBox )) {
+                LabelField( "Application", application.ToString() );
+            }
+        }
+        protected virtual void OnInspectorGUI(GameBase? game) {
+            using (new GUILayout.VerticalScope( EditorStyles.helpBox )) {
+                LabelField( "Game", game?.ToString() ?? "Null" );
+            }
         }
 
         // Helpers
