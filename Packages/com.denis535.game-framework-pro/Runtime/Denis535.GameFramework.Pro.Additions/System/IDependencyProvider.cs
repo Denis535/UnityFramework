db414aae946ba66d23ba4ec057623eb1ae2cd770
Denis535 <dddenisss35@gmail.com>
2025-09-18 08:34:47 +0300

    com.denis535.game-framework-pro

--- Packages/com.denis535.game-framework-pro/Runtime/Denis535.GameFramework.Pro.Additions/System/IDependencyProvider.cs
+++ Packages/com.denis535.game-framework-pro/Runtime/Denis535.GameFramework.Pro.Additions/System/IDependencyProvider.cs
@@ -1,9 +1,8 @@
 #nullable enable
-namespace UnityEngine {
+namespace System {
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using UnityEngine;
 
     public interface IDependencyProvider {
 
@@ -25,7 +24,7 @@ namespace UnityEngine {
         }
         public sealed T RequireDependency<T>(Type type, object? argument = null) {
             var value = this.GetValue( type, argument );
-            Assert.Operation.Message( $"Dependency {typeof( T )} ({argument}) was not found" ).Valid( value.HasValue );
+            Assert.Operation.Message( $"Dependency {type} ({argument}) was not found" ).Valid( value.HasValue );
             return (T) value.Value!;
         }
 
